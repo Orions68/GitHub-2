2e19db16d9fdd1fe713acbce1101015de6b45809
Orions68 <orions68@gmail.com>
2023-03-09 03:11:43 +0000

    Update 09-03-2023

--- Proyecto-1/index.php
+++ Proyecto-1/index.php
@@ -10,6 +10,25 @@ include "includes/header.php";
                 <div id="view1">
                     <br><br><br><br>
                     <h1>Hola Mostro!</h1>
+                    <?php
+ 
+function fibonacci($n)
+{
+    $fibonacci  = [0, 1];
+    echo $fibonacci[0] ."<br>" . $fibonacci[1] . "<br>";
+ 
+  for($i = 2; $i <= $n; $i++)
+    {
+        $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+        echo $fibonacci[3];
+        echo $fibonacci[$i] . "<br>";
+    }
+    print_r ($fibonacci);
+}
+ 
+fibonacci(10);
+ 
+?>
                 </div>
             </div>
         <div class="col-md-1"></div>
