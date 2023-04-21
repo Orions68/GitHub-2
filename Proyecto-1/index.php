b66a913a710f0407595f62fba45556f938794e59
Orions68 <orions68@gmail.com>
2023-04-21 21:26:00 +0100

    Update 21-04-2023_casa

--- Proyecto-1/index.php
+++ Proyecto-1/index.php
@@ -10,23 +10,28 @@ include "includes/header.php";
                 <div id="view1">
                     <br><br><br><br>
                     <h1>Hola Mostro!</h1>
+                    <br><br>
+                    <form action="index.php" method="post">
+                        <label><input type="number" name="number"> Ingresa el Número Hasta el que Quieras Ver la Serie</label>
+                        <br><br>
+                        <input type="submit" value="Muestrame estos Valores">
+                    </form>
                     <?php
- 
-function fibonacci($n)
+ if (isset($_POST["number"]))
 {
+    $n = $_POST["number"];
     $fibonacci  = [0, 1];
-    echo $fibonacci[0] ."<br>" . $fibonacci[1] . "<br>";
+    echo "<h3>$fibonacci[0]</h3><h3>$fibonacci[1]</h3>";
  
-  for($i = 2; $i <= $n; $i++)
+    for($i = 2; $i <= $n; $i++)
     {
         $fibonacci[] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
-        echo $fibonacci[$i] . "<br>";
+        echo "<h3>$fibonacci[$i]</h3>";
         // echo decbin($fibonacci[$i]) . "<br>";
         // echo dechex($fibonacci[$i]) . "<br>";
     }
+    echo "<br><br><br><br>";
 }
- 
-fibonacci(15);
 ?>
                 </div>
             </div>
